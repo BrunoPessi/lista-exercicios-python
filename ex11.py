6a78e93e36a5b630bd8b5fddaff6eba981a5384c
Bruno Pessi <brunoderzi99@gmail.com>
2019-08-15 20:13:12 -0300

    ex11

---
+++ ex11.py
@@ -0,0 +1,6 @@
+# 11 -  Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor.
+
+num = int(input("Digite um numero: "))
+
+num_anterior = num - 1
+print("O numero anterior ao digitado é: ",num_anterior)
