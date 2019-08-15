455f2c5604fbb1c27d73efa83b0e0eadaa72a20c
Bruno Pessi <brunoderzi99@gmail.com>
2019-08-15 20:12:57 -0300

    ex9

---
+++ ex9.py
@@ -0,0 +1,8 @@
+# 9 - faça um método que receba um número X e uma palavra no console e repita x vezes a essa frase.
+
+num = int(input("Digite um numero: "))
+palavra = input("Digite uma palavra a ser repitida: ")
+
+for i in range (num):
+    print(palavra)
+
